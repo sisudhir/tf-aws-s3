17c155b6530a3923619a31f1ffb23bf78717bebd
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-20 15:38:18 +0530

    modified variables

--- vars.tf
+++ vars.tf
@@ -2,17 +2,13 @@ variable "bucket_name" {
 }
 
 variable "acl_value" {
-    default = "private"
 }
 
 variable "aws_access_key" {
-default = "AKIA4UOVTWURT2WVJTFP"
 }
 
 variable "aws_secret_key" {
-default = "d1MjEcoIKFdwD+scQn2HAcJxuOcPkkvElveQJosW"
 }
 
 variable "region" {
-    default = "us-east-1"
 }
